8bea270dd8c6ee54a7e82805067595e04dcaa9f5
lucasmuniz95 <andradelucasmuniz@gmail.com>
2023-09-11 19:44:17 -0300

    primeiro commit

---
+++ src/main/java/com/example/resevasalao/reservasalao/ReservasalaoApplication.java
@@ -0,0 +1,13 @@
+package com.example.resevasalao.reservasalao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReservasalaoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReservasalaoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/resevasalao/reservasalao/ReservasalaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.resevasalao.reservasalao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservasalaoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
